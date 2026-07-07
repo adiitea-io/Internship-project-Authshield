c2a813c9bea543bd1f5915b8d6249d4cc16a5372
adiitea-io <adiitea.yo@gmail.com>
2026-07-07 15:43:13 +0530

    feat: add reusable user service

---
+++ src/services/user.service.js
@@ -0,0 +1,25 @@
+const User = require("../models/user.model");
+
+const createUser = async (userData) => {
+    const user = await User.create(userData);
+
+    return user;
+};
+
+const getUserByEmail = async (email) => {
+    const user = await User.findOne({email});
+
+    return user;
+};
+
+
+const getUserById = async (id) => {
+    const user = await User.findById(id);
+    return user;
+};
+
+module.exports = {
+    createUser,
+    getUserByEmail,
+    getUserById
+};
