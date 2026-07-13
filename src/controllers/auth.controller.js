0457ec7fd593dcf1a48b229193ff03bbe38aac10
adiitea-io <adiitea.yo@gmail.com>
2026-07-13 11:09:48 +0530

    auth controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+import { register } from "../services/auth.service.js";
+
+export const registerUser = async (req, res) => {
+    try {
+        const user = await register(req.body);
+        res.status(201).json({
+            message: "user registered successfully",
+            user
+        });
+    }
+
+    catch (error) {
+        res.status(400).json({
+            message: error.message
+        })
+    }
+}
