b755331df917f2a012fd3cd3a1398e12e0172851
adiitea-io <adiitea.yo@gmail.com>
2026-07-13 18:43:29 +0530

    feat: authentication service

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,8 @@
-import { register } from "../services/auth.service.js";
+import * as authService from "../services/auth.service.js";
 
 export const registerUser= async (req, res) => {
     try {
-        const user = await register(req.body);
+        const user = await authService.register(req.body);
         res.status(201).json({
             message: "user registered successfully",
             user
@@ -15,3 +15,22 @@ export const registerUser= async (req, res) => {
         })
     }
 }
+
+export const loginUser = async (req, res) => {
+    try {
+        const loginResponse = await authService.login(req.body);
+        res.status(200).json(
+            {
+                message: "Login successful",
+                ...loginResponse //everything in loginResponse will be spreaded here, so we can access token and user directly
+            });
+    }
+
+    catch (error) {
+        res.status(401).json({
+            message: error.message
+        })
+    }
+}
+    
+
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,9 +1,10 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { createUser, getUserByEmail } from "./user.service.js";
 
-export const register= async (userData) => {
+export const register = async (userData) => {
     const { name, email, password } = userData;
-    
+
     const existingUser = await getUserByEmail(email);
     if (existingUser) {
         throw new Error("User with this email already exists");
@@ -23,5 +24,37 @@ export const register= async (userData) => {
         name: user.name,
         email: user.email,
         role: user.role
+    };
+}
+
+
+export const login = async (userData) => {
+    const { email, password } = userData;
+
+    const user = await getUserByEmail(email);
+    if (!user) {
+        throw new Error("Invalid credentials");
+    }
+
+    const isPwdValid = await bcrypt.compare(password, user.password);
+    if (!isPwdValid) {
+        throw new Error("Invalid credentials");
+    }
+
+    const token = jwt.sign({    //token is basically what we attach with each request so that we dont need verify user each time 
+        id: user.id,                 //no need name for jwt, its not a unique identifier 
+        role: user.role
+    },
+        process.env.JWT_SECRET, { expiresIn: "1d" });  
+    //jwt.sign() output will be a string 
+        
+    return {
+        token,
+        user: {
+            id: user.id,
+            name: user.name,   //we are returning user tho its in the token cuz its needed in UI, JWT is auth only
+            email: user.email,
+            role: user.role
+        }
     };
 }
