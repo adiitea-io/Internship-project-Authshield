b046422bbeb898ca89452acd21667761cd62349f
adiitea-io <adiitea.yo@gmail.com>
2026-07-14 18:34:36 +0530

    login api route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/auth.controller.js";
+import * as authController from "../controllers/auth.controller.js";
 
 const router = Router();
 
-router.post("/register", registerUser);
+router.post("/register", authController.registerUser);
+router.post("/login", authController.loginUser);
 
 export default router;
+
+
+ 
